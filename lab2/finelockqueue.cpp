f7fe219bcf60744a2adc9fb05e5e0e6c235b6572
starkovsavva <starkovsavva52@gmail.com>
2025-11-17 08:00:01 +0300

    really fine lock now

--- lab2/finelockqueue.cpp
+++ lab2/finelockqueue.cpp
@@ -1,6 +1,7 @@
 #include <mutex>
 #include <memory>
 #include <thread>
+#include <condition_variable>
 
 // Узел очереди
 template<typename T>
@@ -16,7 +17,9 @@ class FineLockQueue {
 private:
     FineNode<T>* head_;
     FineNode<T>* tail_;
-    std::mutex mutex_;
+    std::mutex head_mutex_;
+    std::mutex tail_mutex_;
+    std::condition_variable cv_;
 
 public:
     FineLockQueue() {
@@ -26,6 +29,7 @@ public:
     }
 
     ~FineLockQueue() {
+        std::lock_guard<std::mutex> head_lock(head_mutex_);
         while (head_ != nullptr) {
             FineNode<T>* temp = head_;
             head_ = head_->next;
@@ -38,32 +42,36 @@ public:
 
     void push(T value) {
         FineNode<T>* new_node = new FineNode<T>(std::move(value));
-        std::lock_guard<std::mutex> lock(mutex_);
-        tail_->next = new_node;
-        tail_ = new_node;
+        {
+            std::lock_guard<std::mutex> tail_lock(tail_mutex_);
+            tail_->next = new_node;
+            tail_ = new_node;
+        }
+        cv_.notify_one();
     }
 
     std::shared_ptr<T> try_pop() {
-        std::lock_guard<std::mutex> lock(mutex_);
-        if (head_ == tail_ || head_->next == nullptr) {
-            return nullptr; // Очередь пуста
-        }
-        FineNode<T>* old_head = head_;
-        FineNode<T>* new_head = head_->next;
-        if (!new_head) {
+        std::lock_guard<std::mutex> head_lock(head_mutex_);
+        FineNode<T>* next = head_->next;
+        if (next == nullptr) {
             return nullptr;
         }
-        std::shared_ptr<T> res = new_head->data;
-        head_ = new_head;
-        delete old_head; // Старый фиктивный узел больше не нужен
+        std::shared_ptr<T> res = next->data;
+        FineNode<T>* old_head = head_;
+        head_ = next;
+        delete old_head;
         return res;
     }
 
     std::shared_ptr<T> wait_pop() {
-        std::shared_ptr<T> res;
-        while ((res = try_pop()) == nullptr) {
-            std::this_thread::yield();
-        }
+        std::unique_lock<std::mutex> head_lock(head_mutex_);
+        cv_.wait(head_lock, [&] { return head_->next != nullptr; });
+        FineNode<T>* next = head_->next;
+        std::shared_ptr<T> res = next->data;
+        FineNode<T>* old_head = head_;
+        head_ = next;
+        head_lock.unlock();
+        delete old_head;
         return res;
     }
 
@@ -72,7 +80,7 @@ public:
     }
 
     bool empty() {
-        std::lock_guard<std::mutex> lock(mutex_);
-        return head_ == tail_;
+        std::lock_guard<std::mutex> head_lock(head_mutex_);
+        return head_->next == nullptr;
     }
 };
